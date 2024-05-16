9055a3e3d5a8929961c15da26dada9192f6c26c0
olsohee <dlthgml0108@daum.net>
2024-05-16 11:12:10 +0900

    test: 예약 취소 기능 테스트 작성

--- src/test/java/funfit/pt/schedule/service/ScheduleServiceTest.java
+++ src/test/java/funfit/pt/schedule/service/ScheduleServiceTest.java
@@ -6,12 +6,13 @@ import funfit.pt.rabbitMq.entity.User;
 import funfit.pt.rabbitMq.service.UserService;
 import funfit.pt.relationship.entity.Relationship;
 import funfit.pt.relationship.repository.RelationshipRepository;
-import funfit.pt.schedule.dto.AddScheduleRequest;
+import funfit.pt.schedule.dto.AddAndDeleteScheduleRequest;
 import funfit.pt.schedule.dto.AddScheduleResponse;
 import funfit.pt.schedule.dto.ReadScheduleResponse;
 import funfit.pt.schedule.entity.Schedule;
 import funfit.pt.schedule.repository.ScheduleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,7 @@ import java.time.LocalDateTime;
 import java.time.temporal.TemporalAdjusters;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -36,15 +38,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Transactional
 class ScheduleServiceTest {
 
-    @Autowired private ScheduleRepository scheduleRepository;
-    @Autowired private RelationshipRepository relationshipRepository;
-    @Autowired private ScheduleService scheduleService;
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+    @Autowired
+    private RelationshipRepository relationshipRepository;
+    @Autowired
+    private ScheduleService scheduleService;
 
     @TestConfiguration
     static class TestConfig {
 
-        @Autowired private ScheduleRepository scheduleRepository;
-        @Autowired private RelationshipRepository relationshipRepository;
+        @Autowired
+        private ScheduleRepository scheduleRepository;
+        @Autowired
+        private RelationshipRepository relationshipRepository;
 
         @Bean
         public ScheduleService scheduleService() {
@@ -93,10 +100,10 @@ class ScheduleServiceTest {
         // then
         assertThat(responseDto.getTrainersReservationCount()).isEqualTo(7);
         responseDto.getReservedTimeList().stream()
-                        .forEach(reservationTime -> {
-                            assertThat(reservationTime.isReadUsersReservation()).isTrue();
-                            assertThat(reservationTime.getMemberName()).isEqualTo("member");
-                        });
+                .forEach(reservationTime -> {
+                    assertThat(reservationTime.isReadUsersReservation()).isTrue();
+                    assertThat(reservationTime.getMemberName()).isEqualTo("member");
+                });
     }
 
     @Test
@@ -107,7 +114,7 @@ class ScheduleServiceTest {
         relationshipRepository.save(relationship);
 
         LocalDateTime dateTime = LocalDateTime.of(2024, 1, 1, 18, 00);
-        AddScheduleRequest requestDto = new AddScheduleRequest(dateTime);
+        AddAndDeleteScheduleRequest requestDto = new AddAndDeleteScheduleRequest(dateTime);
 
         // when
         AddScheduleResponse responseDto = scheduleService.addSchedule(requestDto, "member@naver.com");
@@ -136,7 +143,7 @@ class ScheduleServiceTest {
                 relationshipRepository.save(relationship);
 
                 // scheduleService.addSchedule() 호출
-                AddScheduleRequest requestDto = new AddScheduleRequest(dateTime);
+                AddAndDeleteScheduleRequest requestDto = new AddAndDeleteScheduleRequest(dateTime);
                 try {
                     scheduleService.addSchedule(requestDto, memberEmail);
                 } catch (BusinessException e) {
@@ -155,5 +162,22 @@ class ScheduleServiceTest {
                 .toList()
                 .size();
         assertThat(size).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("스케줄 취소 성공")
+    public void deleteScheduleTestSuccess() {
+        // given
+        Relationship relationship = Relationship.create("member@naver.com", "trainer@naver.com", "펀핏짐", 10);
+        relationshipRepository.save(relationship);
+
+        LocalDateTime dateTime = LocalDateTime.of(2024, 1, 1, 18, 00);
+        AddAndDeleteScheduleRequest requestDto = new AddAndDeleteScheduleRequest(dateTime);
+        scheduleService.addSchedule(requestDto, "member@naver.com");
+
+        // then
+        Assertions.assertThat(scheduleRepository.findByRelationshipAndDateTime(relationship, dateTime)).isPresent();
+        scheduleService.deleteSchedule(requestDto, "member@naver.com");
+        Assertions.assertThat(scheduleRepository.findByRelationshipAndDateTime(relationship, dateTime)).isEmpty();
     }
 }
