f31c4b25fe2d0e151093b6491b1ea917f3c3c50b
olsohee <dlthgml0108@daum.net>
2024-05-16 16:28:09 +0900

    refactor: cqrs 적용

--- src/test/java/funfit/pt/diary/service/DiaryServiceTest.java
+++ src/test/java/funfit/pt/diary/service/DiaryServiceTest.java
@@ -1,8 +1,9 @@
 package funfit.pt.diary.service;
 
 import funfit.pt.diary.dto.CreatePostRequest;
-import funfit.pt.diary.dto.CreateDiaryResponse;
 import funfit.pt.diary.entity.Category;
+import funfit.pt.diary.entity.Post;
+import funfit.pt.diary.repository.PostRepository;
 import funfit.pt.exception.ErrorCode;
 import funfit.pt.exception.customException.BusinessException;
 import funfit.pt.relationship.entity.Relationship;
@@ -14,8 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,6 +25,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DiaryServiceTest {
 
     @Autowired private DiaryService diaryService;
+    @Autowired private PostRepository postRepository;
     @Autowired private RelationshipRepository relationshipRepository;
 
     @Test
@@ -39,13 +40,15 @@ class DiaryServiceTest {
         imageUrls.add("url1");
         imageUrls.add("url2");
         CreatePostRequest createPostRequest = new CreatePostRequest("오늘 운동일지입니다.", imageUrls);
-        CreateDiaryResponse responseDto = diaryService.createPtLog(relationship.getId(), createPostRequest, "trainer@naver.com");
+        long postId = diaryService.createPost(relationship.getId(), "수업일지", createPostRequest, "trainer@naver.com");
 
         // then
-        assertThat(responseDto.getWriterEmail()).isEqualTo("trainer@naver.com");
-        assertThat(responseDto.getContent()).isEqualTo("오늘 운동일지입니다.");
-        assertThat(responseDto.getCategory()).isEqualTo(Category.PT_LOG.getName());
-        assertThat(responseDto.getImageUrls().size()).isEqualTo(2);
+        Post savedPost = postRepository.findById(postId).get();
+        assertThat(savedPost.getWriterEmail()).isEqualTo("trainer@naver.com");
+        assertThat(savedPost.getContent()).isEqualTo("오늘 운동일지입니다.");
+        assertThat(savedPost.getCategory()).isEqualTo(Category.PT_LOG);
+        assertThat(savedPost.getImages().size()).isEqualTo(2);
+        assertThat(savedPost.getComments().size()).isEqualTo(0);
     }
 
     @Test
@@ -59,7 +62,7 @@ class DiaryServiceTest {
         CreatePostRequest createPostRequest = new CreatePostRequest("오늘 운동일지입니다.", null);
 
         // then
-        Assertions.assertThat(assertThrows(BusinessException.class, () -> diaryService.createPtLog(relationship.getId(), createPostRequest, "member@naver.com"))
+        Assertions.assertThat(assertThrows(BusinessException.class, () -> diaryService.createPost(relationship.getId(), "수업일지", createPostRequest, "member@naver.com"))
                 .getErrorCode()).isEqualTo(ErrorCode.UNAUTHORIZED_CREATE_PT_LOG);
     }
 
@@ -75,13 +78,15 @@ class DiaryServiceTest {
         imageUrls.add("url1");
         imageUrls.add("url2");
         CreatePostRequest createPostRequest = new CreatePostRequest("오늘의 식단!", imageUrls);
-        CreateDiaryResponse responseDto = diaryService.createDiary(relationship.getId(), createPostRequest, "member@naver.com");
+        long postId = diaryService.createPost(relationship.getId(), "다이어리", createPostRequest, "member@naver.com");
 
         // then
-        assertThat(responseDto.getWriterEmail()).isEqualTo("member@naver.com");
-        assertThat(responseDto.getContent()).isEqualTo("오늘의 식단!");
-        assertThat(responseDto.getCategory()).isEqualTo(Category.DIARY.getName());
-        assertThat(responseDto.getImageUrls().size()).isEqualTo(2);
+        Post savedPost = postRepository.findById(postId).get();
+        assertThat(savedPost.getWriterEmail()).isEqualTo("member@naver.com");
+        assertThat(savedPost.getContent()).isEqualTo("오늘의 식단!");
+        assertThat(savedPost.getCategory()).isEqualTo(Category.DIARY);
+        assertThat(savedPost.getImages().size()).isEqualTo(2);
+        assertThat(savedPost.getComments().size()).isEqualTo(0);
     }
 
     @Test
@@ -95,7 +100,7 @@ class DiaryServiceTest {
         CreatePostRequest createPostRequest = new CreatePostRequest("오늘의 식단!", null);
 
         // then
-        Assertions.assertThat(assertThrows(BusinessException.class, () -> diaryService.createDiary(relationship.getId(), createPostRequest, "trainer@naver.com"))
-                .getErrorCode()).isEqualTo(ErrorCode.UNAUTHORIZED_CREATE_PT_LOG);
+        Assertions.assertThat(assertThrows(BusinessException.class, () -> diaryService.createPost(relationship.getId(), "다이어리", createPostRequest, "trainer@naver.com"))
+                .getErrorCode()).isEqualTo(ErrorCode.UNAUTHORIZED_CREATE_DIARY);
     }
 }
